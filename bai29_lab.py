3741d84881a62066420fec4f5f53bf9323625936
NguyenMinhNhut1425 <mnhut1425@gmail.com>
2024-09-20 16:01:20 +0700

    bai29_lab.py

--- bai29_lab.py
+++ bai29_lab.py
@@ -7,6 +7,8 @@ Created on Sat Sep 14 14:29:20 2024
 
 N = int(input("Nhập vào số nguyên dương N: "))
 tong = 0
+while N <= 0:
+    N = int(input("N phải là số nguyên dương")
 for i in str(N):
     tong += int(i)
 print(f"Tổng các chữ số của N là: {tong}")
